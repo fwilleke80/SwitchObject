e2093503a43ddb836028ed07d0c72d6ccafd9bfa
Frank Willeke <frank.willeke@laubwerk.com>
2017-02-25 15:35:48 +0100

    Added resource files

---
+++ res/c4d_symbols.h
@@ -0,0 +1,13 @@
+enum
+{
+	// string table definitions
+	IDS_OSWITCHOBJ	= 10000,
+	IDS_SWITCH_DROPDOWN,
+	IDS_SWITCH_NONE,
+	IDS_SWITCH_INHERITNAME,
+	IDS_SWITCH_OBJNAME,
+	IDS_SWITCH_REMOTELINK,
+
+// End of symbol definition
+  _DUMMY_ELEMENT_
+};
---
+++ res/description/oswitchobject.res
@@ -0,0 +1,10 @@
+CONTAINER oswitchobject
+{
+	NAME oswitchobject;
+	INCLUDE Obase;
+
+	GROUP ID_OBJECTPROPERTIES
+	{
+		DEFAULT 1;
+	}
+}
